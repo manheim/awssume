7c8a1e2120889b14240a560afe4967f6565290c3
reppard <reppardwalker@gmail.com>
2016-03-31 13:51:00 -0400

    style(awssume.rb): use single quotes

--- lib/awssume.rb
+++ lib/awssume.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk'
-require "awssume/version"
-require "awssume/adapter/aws_client"
+require 'awssume/version'
+require 'awssume/adapter/aws_client'
 
 module Awssume
   # Your code goes here...
